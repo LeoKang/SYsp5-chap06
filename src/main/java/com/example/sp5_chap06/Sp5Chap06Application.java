8ef4787ea425d2e757c5312ceb1c335bea93f19b
SYU <xeungjin@gmail.com>
2025-03-28 11:08:05 +0900

    ~ 147 page, 커스텀 메서드

--- src/main/java/com/example/sp5_chap06/Sp5Chap06Application.java
+++ src/main/java/com/example/sp5_chap06/Sp5Chap06Application.java
@@ -2,6 +2,7 @@ package com.example.sp5_chap06;
 
 import com.example.sp5_chap06.config.AppCtx;
 import com.example.sp5_chap06.spring.Client;
+import com.example.sp5_chap06.spring.Client2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,7 +14,8 @@ public class Sp5Chap06Application {
 		//SpringApplication.run(Sp5Chap06Application.class, args);
 		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
 
-		Client client = ctx.getBean(Client.class);
+//		Client client = ctx.getBean(Client.class);
+		Client2 client = ctx.getBean(Client2.class);
 		client.send();
 
 		ctx.close();
--- src/main/java/com/example/sp5_chap06/config/AppCtx.java
+++ src/main/java/com/example/sp5_chap06/config/AppCtx.java
@@ -1,6 +1,7 @@
 package com.example.sp5_chap06.config;
 
 import com.example.sp5_chap06.spring.Client;
+import com.example.sp5_chap06.spring.Client2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,13 @@ public class AppCtx {
     @Bean
     public Client client() {
         Client client = new Client();
+        client.setHost("host");
+        return client;
+    }
+
+    @Bean(initMethod = "connect", destroyMethod = "close")
+    public Client2 client2() {
+        Client2 client = new Client2();
         client.setHost("host");
         return client;
     }
